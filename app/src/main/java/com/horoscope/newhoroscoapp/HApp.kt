27a426e9f02bb981a2532cc3ec15f5647e58f0e4
Rodrigo Martin <rodrigorodgms@gmail.com>
2023-09-24 13:12:35 -0600

    add DaggerHilt

---
+++ app/src/main/java/com/horoscope/newhoroscoapp/HApp.kt
@@ -0,0 +1,8 @@
+package com.horoscope.newhoroscoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HApp : Application() {
+}
--- app/src/main/java/com/horoscope/newhoroscoapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/horoscope/newhoroscoapp/ui/horoscope/HoroscopeFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.horocope.newhoroscoapp.databinding.FragmentHoroscopeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
