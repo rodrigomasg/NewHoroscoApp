505984e89cde469494118c8a2d84c46365fb3477
Rodrigo Martin <rodrigorodgms@gmail.com>
2023-09-24 13:42:42 -0600

    model and res Horoscope

--- app/src/main/java/com/horoscope/newhoroscoapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/horoscope/newhoroscoapp/ui/horoscope/HoroscopeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.horocope.newhoroscoapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
---
+++ app/src/main/java/com/horoscope/newhoroscoapp/ui/horoscope/HoroscopeViewModel.kt
@@ -0,0 +1,10 @@
+package com.horoscope.newhoroscoapp.ui.horoscope
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class HoroscopeViewModel @Inject constructor() : ViewModel() {
+
+}
